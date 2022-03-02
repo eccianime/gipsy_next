de20a90fc513d960497555c620f98cd646547f98
Hallyson Miranda <hallyson.miranda@insightlab.ufc.br>
2022-03-02 16:14:01 -0300

    adicionando roteamento para gipsyy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/:path*',
+        destination: 'https://stgfrontbr.gipsyy.com/:path*',
+      }
+    ]
+  },
+}
